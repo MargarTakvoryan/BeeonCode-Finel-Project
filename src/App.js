4b9d805b7e03c98c7d2425cb3db6078fac358bd0
MargarTakvoryan <gtakvoryn@gmail.com>
2023-05-10 23:45:21 +0400

    Add Gender Componet and finished searche componet styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 // import './App.css';
+import { Route,  Routes } from "react-router-dom";
+import Gender from "./Componets/Gender/Gender";
+import Search from "./Componets/Search/Search";
+import Man from "./Componets/Gender/Man/Man";
+import Woman from "./Componets/Gender/Woman/Woman";
 
 function App() {
   return (
     <div className="App">
-
+      <Search />
+      
+      <Routes>
+        <Route element={<Gender />}  path={'/'}>
+          <Route element={<Man/>}  index={true}/>
+          <Route element={<Woman/>} path="/woman"/>
+        </Route>
+      </Routes>
+      
     </div>
   );
 }
